d03212337852d0a3e6e9370ac484987e6ccfa255
Carlos Sales <carlossalesti@gmail.com>
2017-10-08 18:15:23 -0300

    em andamento

--- BotApplication1/BotApplication1/Dialogs/RootLuisDialog.cs
+++ BotApplication1/BotApplication1/Dialogs/RootLuisDialog.cs
@@ -17,57 +17,58 @@ namespace BotApplication1.Dialogs
         [LuisIntent("None")]
         public async Task NoneAsync(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("Iniciando Atendente Virtual...");
-            context.Wait(MessageReceivedAsync);
+            await context.PostAsync("Desculpe, não entendi o que disse, pode reformular a pergunta ?");
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Oi")]
         public async Task OiAsync(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("Oi, tudo legal.");
+            Activity replyToConversation = ShowButtons(context, "Oi, sou seu Atendente Virtual. Escolha uma opção...!");
+            await context.PostAsync(replyToConversation);
             context.Wait(MessageReceivedAsync);
         }
 
         [LuisIntent("Sobre")]
         public async Task SobreAsync(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("Eu me chamo RoboZika. Sou um robô de combate ao mosquito Aedes. No momento ainda estou em fase de aprendizado.");
-            context.Wait(MessageReceivedAsync);
+            await context.PostAsync("Eu sou um robô de combate ao mosquito Aedes. Faço parta da Aedes Patrulha. No momento ainda estou em fase de aprendizado.");
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Tchau")]
         public async Task TchauAsync(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("Vlw, grande abraço, até a próxima.");
-            context.Wait(MessageReceivedAsync);
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Dengue")]
         public async Task DengueAsync(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("A Dengue é uma doença viral transmitida por um mosquito. O Aedes Aegypti. Agora cuidado, pessoas infectadas com o vírus pela segunda vez têm um risco significativamente maior de desenvolver doença GRAVE.");
-            context.Wait(MessageReceivedAsync);
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Chikungunya")]
         public async Task ChikungunyaAsync(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("Rapaz, êta nome difícil...rsrsrs . Mas a Chikungunya assim como a Dengue também é transmitida pelo mosquito Aedes.");
-            context.Wait(MessageReceivedAsync);
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Zika")]
         public async Task ZikaAsync(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("Pense num problema!!! O Zika Vírus já é considerado uma ameaça à saúde mundial. É TAMBÉM transmitido pelo tal do mosquito Aedes.");
-            context.Wait(MessageReceivedAsync);
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Aedes")]
         public async Task AedesAsync(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("O Aedes Aegypti é o mosquito transmissor da Dengue, Zika, Chikungunya e da Febre amarela urbana. Menor do que os mosquitos comuns, é preto com listras brancas no tronco, na cabeça e nas pernas.");
-            context.Wait(MessageReceivedAsync);
+            context.Wait(MessageReceived);
         }
 
         public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
